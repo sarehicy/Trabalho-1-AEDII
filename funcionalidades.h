79741042d5781571647789a7b6d6908f2b37be7b
Sarah Meicy <133773435+sarehicy@users.noreply.github.com>
2026-06-05 16:20:12 -0300

    funcionalidades 1 à 5 funcionando corretamente!

---
+++ funcionalidades.h
@@ -0,0 +1,42 @@
+#ifndef FUNCIONALIDADES_H
+#define FUNCIONALIDADES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "auxiliares.h"
+#include "cabecalhos.h"
+#include "escrever_arq.h"
+#include "ler_arq.h"
+#include "funcionalidades.h"
+#include "registros.h"
+#include "structs.h"
+
+
+// --- Funcionalidade 1 do programa ---
+// Lê um arquivo csv e gera um arquivo bin
+void funcUm(dadosHeader *dados);
+
+// --- Funcionalidade 2 do programa ---
+// Dado um arquivo .bin, imprime seus registros
+void funcDois();
+
+// --- Funcionalidade 3 do programa ---
+// Pesquisa e retorna, se econtrar, registros com os campos especificados na busca
+void funcTres();
+
+// --- Funcionalidade 4 do programa ---
+// Pesquisa e retorna, se encontrar, registro com o RRN específicado
+void funcQuatro();
+
+void funcCinco();
+
+// --- Funcionalidade 6 do programa ---
+// Busca por campo, onde se um dos campos buscados for "codEstacao" faz uma busca indexada
+void funcSeis();
+
+/*Remove todos os registros que correspondem a um determinado critério de busca*/
+void funcSete(dadosHeader *dados);
+//testandoooo
+
+#endif
