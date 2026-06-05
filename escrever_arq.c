79cc0d325591c5ba135af570c9b4dd57f59a279e
Sarah Meicy <133773435+sarehicy@users.noreply.github.com>
2026-06-05 16:09:54 -0300

    Funções que escrevem em arquivos

---
+++ escrever_arq.c
@@ -0,0 +1,60 @@
+#include "escrever_arq.h"
+
+void escreverRegistro(FILE *arq, reg *registro){
+    verificarArq(arq);
+
+    /*    #   Escrevendo Campos do Registro  #  */
+
+    fwrite(&(registro->rem), sizeof(char), 1, arq);
+    
+    fwrite(&(registro->prox), sizeof(int), 1, arq);
+
+    fwrite(&(registro->codEstacao), sizeof(int), 1, arq);
+
+    fwrite(&(registro->codLinha), sizeof(int), 1, arq); 
+
+    fwrite(&(registro->codProxEstacao), sizeof(int), 1, arq);
+    
+    fwrite(&(registro->distProxEstacao), sizeof(int), 1, arq);
+
+    fwrite(&(registro->codLinhaInteg), sizeof(int), 1, arq);
+    
+    fwrite(&(registro->codEstacaoInteg), sizeof(int), 1, arq);
+
+    fwrite(&(registro->tamNomeEstacao), sizeof(int), 1, arq);
+
+    fwrite(&(registro->nomeEstacao), sizeof(char), registro->tamNomeEstacao, arq);
+
+    fwrite(&(registro->tamNomeLinha), sizeof(int), 1, arq);
+
+    fwrite(&(registro->nomeLinha), sizeof(char), registro->tamNomeLinha, arq);
+
+    /*   #   Preenchendo com Lixo    # */
+
+    int qtdBytesLixo = tamRegistro - (bytesFixos + registro->tamNomeEstacao + registro->tamNomeLinha);
+    char lixoSign = '$';
+
+    //  Enquanto houver espaço vazio, escreve '$' no arquivo
+    while(qtdBytesLixo > 0){
+        fwrite(&lixoSign, sizeof(char), 1, arq);
+
+        qtdBytesLixo--;
+    }
+}
+
+void escreverHeader(FILE *arq, header *cabecalho){
+    verificarArq(arq); 
+    fseek(arq, 0, SEEK_SET);
+
+    // Escreve os dados do cabeçalho da memória no arquivo
+
+    fwrite(&(cabecalho->status), sizeof(char), 1, arq);
+
+    fwrite(&(cabecalho->topo), sizeof(int), 1, arq);
+    
+    fwrite(&(cabecalho->proxRRN), sizeof(int), 1, arq);
+    
+    fwrite(&(cabecalho->totalEstacoes), sizeof(int), 1, arq);
+    
+    fwrite(&(cabecalho->totalPares), sizeof(int), 1, arq);
+}
---
+++ escrever_arq.h
@@ -0,0 +1,19 @@
+#ifndef ESCREVER_ARQ_H
+#define ESCREVER_ARQ_H
+
+#include <stdio.h>
+#include "auxiliares.h"
+#include "cabecalhos.h"
+#include "escrever_arq.h"
+#include "ler_arq.h"
+#include "funcionalidades.h"
+#include "registros.h"
+#include "structs.h"
+
+/* Escreve registro (struct) no arquivo binário */
+void escreverRegistro(FILE *arq, reg *registro);
+
+/* Escreve o cabeçalho no arquivo binário   */
+void escreverHeader(FILE *arq, header *cabecalho);
+
+#endif
