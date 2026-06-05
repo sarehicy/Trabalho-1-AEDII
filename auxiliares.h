480dd806f1a2a054fa195aee3b2bbc5e0c6c8b37
Sarah Meicy <133773435+sarehicy@users.noreply.github.com>
2026-06-05 16:17:42 -0300

    Funções auxiliares

---
+++ auxiliares.h
@@ -0,0 +1,50 @@
+#ifndef AUXILIARES_H
+#define AUXILIARES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "auxiliares.h"
+#include "cabecalhos.h"
+#include "escrever_arq.h"
+#include "ler_arq.h"
+#include "funcionalidades.h"
+#include "registros.h"
+#include "structs.h"
+
+/* Lê critérios da busca e guarda em um vetor */
+void montarBusca(char **linha, int qtdCampos);
+
+/* Desaloca strings dentro de um vetor*/
+void desalocaVetorDePonteiros(char **v, int n);
+
+void ScanQuoteString(char *str);
+
+/* Imprime os campos inteiros do registro*/
+void printInt(int n);
+
+/* Imprime os campos string do registro*/
+void printString(char string[], int tamanho);
+
+/* Move o ponteiro do arquivo até o registro de RRN desejado*/
+void movePonteiroRRN(FILE *arq, int rrn);
+
+
+/* Lê e descarta a primeira linha do arquivo (header) */
+void pularCabecalhoCsv(FILE *arq);
+
+void BinarioNaTela(char *arquivo);
+
+char check_eof(FILE* f);
+
+// Faz uma busca em um arquivo indice, retorna o RRN do codEstacao
+
+void ordenarIndiceHeap(regIndex *vetRegistroIndex, int n);
+void heapifyIndice(regIndex *vetRegistroIndex, int n, int i);
+void troca(regIndex *a, regIndex *b);
+void printArray(int arr[], int n);
+
+int buscaIndexada(FILE *arq, int codEstacao);
+
+#endif
