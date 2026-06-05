b8389180e6f1fdca367b7da9762e9cc7fab5cd43
Sarah Meicy <133773435+sarehicy@users.noreply.github.com>
2026-06-05 15:52:43 -0300

    Add files via upload

---
+++ ler_arq.h
@@ -0,0 +1,28 @@
+#ifndef LER_ARQ_H
+#define LER_ARQ_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "auxiliares.h"
+#include "cabecalhos.h"
+#include "escrever_arq.h"
+#include "ler_arq.h"
+#include "funcionalidades.h"
+#include "registros.h"
+#include "structs.h"
+
+/* Verifica ponteiro do arquivo */
+void verificarArq(FILE *arq);
+
+/* Lê um registro do arquivo .csv e monta um registro na memória (struct)*/
+void montarRegistro(FILE *arq, reg *registro);
+
+/* Lê registro do arquivo .bin e monta um registro na memória (struct).
+ Retorna 0 se a leitura for bem sucedida, retorna 1 se não conseguir ler*/
+int lerRegistro(FILE *arq, reg *registro);
+
+/* Lê cabeçalho do arquivo binario e verifica consistência do arquivo*/
+int lerCabecalho(FILE *arq, header *cabecalho);
+
+#endif
